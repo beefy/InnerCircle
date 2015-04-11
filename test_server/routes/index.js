7061e2f36fc74191111638dbcd2e4ca817b88fd1
unknown <nate.schultz@outlook.com>
2015-04-11 05:21:31 -0400

    variable renaming

--- test_server/routes/index.js
+++ test_server/routes/index.js
@@ -2,33 +2,33 @@ var mongoose = require("mongoose");
 var people = mongoose.model('name');
 
 exports.getPerson = function (req, res) {
-    people.findOne({ _id: req.params.id}, function(err, gotProjects) {
+    people.findOne({ _id: req.params.id}, function(err, gotPeople) {
                 if (err) {
                     console.log(err)
                 } else {
-                    res.send(gotProjects);
+                    res.send(gotPeople);
                 }
     });
 }
 
 exports.getPeople = function (req, res) {
-    people.find({}, function (err, gotProjects) {
+    people.find({}, function (err, gotPeople) {
         if (err) {
             console.log(err)
         } else {
-            res.send(gotProjects);
+            res.send(gotPeople);
         }
     });
 };
 
 exports.postPerson = function (req, res) {
-    var project = new people({
+    var person = new people({
         name: req.body.name,
         lat: req.body.lat,
         long: req.body.lat
     });
 
-    project.save(function () {
+    person.save(function () {
         res.send("Saved " + req.body.name)
     });
 };
