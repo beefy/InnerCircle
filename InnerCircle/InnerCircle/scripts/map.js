a422ff37a046f809557748c6dca27a99731e48b9
unknown <nate.schultz@outlook.com>
2015-04-11 19:40:32 -0400

    marker symbols change onclick

--- InnerCircle/InnerCircle/scripts/map.js
+++ InnerCircle/InnerCircle/scripts/map.js
@@ -1,108 +1,6 @@
 ﻿L.mapbox.accessToken = 'pk.eyJ1IjoibnNjaHVsdHoxNCIsImEiOiJUdXJTc0EwIn0.GDGkKBAERoUmL9RXrmP5bQ';
-
-var map = L.mapbox.map('mapholder', 'examples.map-i86nkdio')
-    .setView([38.91338, -77.03236], 13);
-
-//user
-var user = L.mapbox.featureLayer({
-    // this feature is in the GeoJSON format: see geojson.org
-    // for the full specification
-    type: 'Feature',
-    geometry: {
-        type: 'Point',
-        // coordinates here are in longitude, latitude order because
-        // x, y is the standard for GeoJSON and many formats
-        coordinates: [
-          -77.03221142292,
-          38.913371603574
-        ]
-    },
-    properties: {
-        title: 'Peregrine Espresso',
-        description: '1718 14th St NW, Washington, DC',
-        // one can customize markers by adding simplestyle properties
-        // https://www.mapbox.com/guides/an-open-platform/#simplestyle
-        'marker-size': 'large',
-        'marker-color': '#556EFA',
-        'marker-symbol': 'circle-stroked'
-    }
-
-}).addTo(map);
-
-//var bob = L.mapbox.featureLayer({
-//    // this feature is in the GeoJSON format: see geojson.org
-//    // for the full specification
-//    type: 'Feature',
-//    geometry: {
-//        type: 'Point',
-//        // coordinates here are in longitude, latitude order because
-//        // x, y is the standard for GeoJSON and many formats
-//        coordinates: [
-//          -77.04221142292,
-//          38.923371603574
-//        ]
-//    },
-//    properties: {
-//        title: 'Peregrine Espresso',
-//        description: '1718 14th St NW, Washington, DC',
-//        // one can customize markers by adding simplestyle properties
-//        // https://www.mapbox.com/guides/an-open-platform/#simplestyle
-//        'marker-size': 'large',
-//        'marker-color': '#E87E38',
-//        'marker-symbol': 'circle-stroked'
-//    }
-
-//}).addTo(map);
-
-//var sam = L.mapbox.featureLayer({
-//    // this feature is in the GeoJSON format: see geojson.org
-//    // for the full specification
-//    type: 'Feature',
-//    geometry: {
-//        type: 'Point',
-//        // coordinates here are in longitude, latitude order because
-//        // x, y is the standard for GeoJSON and many formats
-//        coordinates: [
-//          -77.04721142292,
-//          38.913371603574
-//        ]
-//    },
-//    properties: {
-//        title: 'Peregrine Espresso',
-//        description: '1718 14th St NW, Washington, DC',
-//        // one can customize markers by adding simplestyle properties
-//        // https://www.mapbox.com/guides/an-open-platform/#simplestyle
-//        'marker-size': 'large',
-//        'marker-color': '#E87E38',
-//        'marker-symbol': 'circle-stroked'
-//    }
-
-//}).addTo(map);
-
-//var sandy = L.mapbox.featureLayer({
-//    // this feature is in the GeoJSON format: see geojson.org
-//    // for the full specification
-//    type: 'Feature',
-//    geometry: {
-//        type: 'Point',
-//        // coordinates here are in longitude, latitude order because
-//        // x, y is the standard for GeoJSON and many formats
-//        coordinates: [
-//          -77.04,
-//          38.973371603574
-//        ]
-//    },
-//    properties: {
-//        title: 'Peregrine Espresso',
-//        description: '1718 14th St NW, Washington, DC',
-//        // one can customize markers by adding simplestyle properties
-//        // https://www.mapbox.com/guides/an-open-platform/#simplestyle
-//        'marker-size': 'large',
-//        'marker-color': '#E87E38',
-//        'marker-symbol': 'circle-stroked'
-//    }
-
-//}).addTo(map);
+var map = L.mapbox.map('map', 'examples.h186knp8')
+    .setView([37.9, -77], 6);
 
 var geoJson = [{
     type: 'Feature',
@@ -111,7 +9,7 @@ var geoJson = [{
         coordinates: [-77, 37.9]
     },
     properties: {
-        title: 'Bob',
+        title: 'Sandy',
         'marker-size': 'large',
         'marker-color': '#E87E38',
         'marker-symbol': 'circle-stroked'
@@ -121,10 +19,10 @@ var geoJson = [{
       type: 'Feature',
       geometry: {
           type: 'Point',
-          coordinates: [-77.04721142292, 38.913371603574]
+          coordinates: [-78, 36.5]
       },
       properties: {
-          title: 'Mark',
+          title: 'Bob',
           'marker-size': 'large',
           'marker-color': '#E87E38',
           'marker-symbol': 'circle-stroked'
@@ -134,47 +32,46 @@ var geoJson = [{
       type: 'Feature',
       geometry: {
           type: 'Point',
-          coordinates: [-77.04, 38.973371603574]
+          coordinates: [-80, 36.5]
       },
       properties: {
-          title: 'Sandy',
+          title: 'Albert',
           'marker-size': 'large',
           'marker-color': '#E87E38',
           'marker-symbol': 'circle-stroked'
       }
-  }
-
-];
+  },
+  {
+      type: 'Feature',
+      geometry: {
+          type: 'Point',
+          coordinates: [-78, 33]
+      },
+      properties: {
+          title: 'user',
+          'marker-size': 'large',
+          'marker-color': '#556EFA',
+          'marker-symbol': 'circle-stroked'
+      }
+  }];
 
 var myLayer = L.mapbox.featureLayer().addTo(map);
 
 myLayer.setGeoJSON(geoJson);
 
-function resetMarkers() {
+function resetColors() {
     for (var i = 0; i < geoJson.length; i++) {
-        geoJson[i].properties['marker-color'] = geoJson[i].properties['old-color'] ||
-            geoJson[i].properties['marker-color'];
-
-       // geoJson[i].properties['marker-symbol'] = geoJson[i].properties['old-symbol'] ||
-        //    geoJson[i].properties['marker-symbol'];
+        geoJson[i].properties['marker-symbol'] = geoJson[i].properties['old-symbol'] ||
+            geoJson[i].properties['marker-symbol'];
     }
     myLayer.setGeoJSON(geoJson);
 }
 
 myLayer.on('click', function (e) {
-    resetMarkers();
-    e.layer.feature.properties['old-color'] = e.layer.feature.properties['marker-color'];
-    //e.layer.feature.properties['old-symbol'] = e.layer.feature.properties['marker-symbol'];
-    e.layer.feature.properties['marker-color'] = '#ff8888';
-    //e.layer.feature.properties['marker-symbol'] = 'circle';
+    resetColors();
+    e.layer.feature.properties['old-symbol'] = e.layer.feature.properties['marker-symbol'];
+    e.layer.feature.properties['marker-symbol'] = 'circle';
     myLayer.setGeoJSON(geoJson);
 });
 
-myLayer.on('click', resetMarkers);
-
-//L.mapbox.toggle(map.zoomControl, this);
-
-//map.getContainer().querySelector('#zoomer').onclick = function () {
-//    toggle(map.zoomControl, this);
-//    return false;
-//};
+map.on('click', resetColors);
