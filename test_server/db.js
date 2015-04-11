422323927a0157e84e21cdcf5ee596b7bcd6d6d9
Derek Alonso <futuredominosemployee@gmail.com>
2015-04-11 02:03:12 -0400

    Revert "small change"
    This reverts commit 6ef6dbb7acbdd88204736625606bc09d0ad6eb1c.
    
    Conflicts:
    	test_server/db.js

--- test_server/db.js
+++ test_server/db.js
@@ -8,10 +8,13 @@ var db_url = "mongodb://localhost:27017/researchMe",
 var projectSchema = new Schema({
     id: ObjectId,
 <<<<<<< HEAD
+<<<<<<< HEAD
     userLat: { type: String, required: true },
     userLong: { type: String, required: true },
     date: { type: Date, default: Date.now }
 =======
+=======
+>>>>>>> parent of 6ef6dbb... small change
     title: {type: String, required: true},
     date: {type: Date, default: Date.now}
 >>>>>>> parent of 6ef6dbb... small change
