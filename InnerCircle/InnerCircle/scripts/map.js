a0c8ef2e6897ded1987b12e41ea4a7180d47b3e0
unknown <nate.schultz@outlook.com>
2015-04-12 06:54:13 -0400

    location working

--- InnerCircle/InnerCircle/scripts/map.js
+++ InnerCircle/InnerCircle/scripts/map.js
@@ -2,100 +2,116 @@
 var userlat = 60;
 var geolocate;
 
-L.mapbox.accessToken = 'pk.eyJ1IjoibnNjaHVsdHoxNCIsImEiOiJUdXJTc0EwIn0.GDGkKBAERoUmL9RXrmP5bQ';
-var map = L.mapbox.map('map', 'examples.h186knp8')
-    .setView([userlong, userlat], 6);
 
-var geoJson = [{
-    type: 'Feature',
-    geometry: {
-        type: 'Point',
-        coordinates: [0, 0]
-    },
-    properties: {
-        title: 'NaN',
-        icon: {
-            "iconUrl": "../images/logo.png",
-            "iconSize": [0, 0],
-            "iconAnchor": [10, 10],
-            "popupAnchor": [0, -55],
-            "className": "dot"
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
+}
+function showPosition(position) {
+    userlat = position.coords.latitude;
+    userlong = position.coords.longitude;
+    loadMap();
+}
+
+function loadMap() {
+    L.mapbox.accessToken = 'pk.eyJ1IjoibnNjaHVsdHoxNCIsImEiOiJUdXJTc0EwIn0.GDGkKBAERoUmL9RXrmP5bQ';
+    var map = L.mapbox.map('map', 'examples.h186knp8')
+        .setView([userlat, userlong], 6);
+
+    var geoJson = [{
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [0, 0]
         },
-        visibility: 'hidden',
-        display: 'none'
-    }
-},
-{
-    type: 'Feature',
-    geometry: {
-        type: 'Point',
-        coordinates: [-77, 37.9]
-    },
-    properties: {
-        title: 'Sandy',
-        icon: {
-            "iconUrl": "../images/logo.png",
-            "iconSize": [25, 25],
-            "iconAnchor": [10, 10],
-            "popupAnchor": [0, -55],
-            "className": "dot"
+        properties: {
+            title: 'NaN',
+            icon: {
+                "iconUrl": "../images/logo.png",
+                "iconSize": [0, 0],
+                "iconAnchor": [10, 10],
+                "popupAnchor": [0, -55],
+                "className": "dot"
+            },
+            visibility: 'hidden',
+            display: 'none'
         }
-    }
-},
-  {
-      type: 'Feature',
-      geometry: {
-          type: 'Point',
-          coordinates: [-78, 36.5]
-      },
-      "properties": {
-          title: 'Bob',
-          icon: {
-              "iconUrl": "../images/logo.png",
-              "iconSize": [25, 25],
-              "iconAnchor": [10, 10],
-              "popupAnchor": [0, -55],
-              "className": "dot"
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-77, 37.9]
+        },
+        properties: {
+            title: 'Sandy',
+            icon: {
+                "iconUrl": "../images/logo.png",
+                "iconSize": [25, 25],
+                "iconAnchor": [10, 10],
+                "popupAnchor": [0, -55],
+                "className": "dot"
+            }
+        }
+    },
+      {
+          type: 'Feature',
+          geometry: {
+              type: 'Point',
+              coordinates: [-78, 36.5]
+          },
+          "properties": {
+              title: 'Bob',
+              icon: {
+                  "iconUrl": "../images/logo.png",
+                  "iconSize": [25, 25],
+                  "iconAnchor": [10, 10],
+                  "popupAnchor": [0, -55],
+                  "className": "dot"
+              }
           }
-      }
 
-  },
-  {
-      type: 'Feature',
-      geometry: {
-          type: 'Point',
-          coordinates: [-80, 36.5]
       },
-      properties: {
-          title: 'Albert',
-          icon: {
-              "iconUrl": "../images/logo.png",
-              "iconSize": [25, 25],
-              "iconAnchor": [10, 10],
-              "popupAnchor": [0, -55],
-              "className": "dot"
+      {
+          type: 'Feature',
+          geometry: {
+              type: 'Point',
+              coordinates: [-80, 36.5]
+          },
+          properties: {
+              title: 'Albert',
+              icon: {
+                  "iconUrl": "../images/logo.png",
+                  "iconSize": [25, 25],
+                  "iconAnchor": [10, 10],
+                  "popupAnchor": [0, -55],
+                  "className": "dot"
+              }
           }
-      }
-  },
-  {
-      type: 'Feature',
-      geometry: {
-          type: 'Point',
-          coordinates: [userlong, userlat]
       },
-      properties: {
-          title: 'user',
-          icon: {
-              "iconUrl": "../images/logo.png",
-              "iconSize": [50, 50],
-              "iconAnchor": [10, 10],
-              "popupAnchor": [0, -55],
-              "className": "dot"
+      {
+          type: 'Feature',
+          geometry: {
+              type: 'Point',
+              coordinates: [userlong, userlat]
+          },
+          properties: {
+              title: 'user',
+              icon: {
+                  "iconUrl": "../images/logo.png",
+                  "iconSize": [50, 50],
+                  "iconAnchor": [10, 10],
+                  "popupAnchor": [0, -55],
+                  "className": "dot"
+              }
           }
-      }
-  }];
+      }];
 
-var myLayer = L.mapbox.featureLayer().bindPopup('<div class="popup"><p>Hey, Bob</p><button class = "way pure-button">On my way</button><br/><button class = "here pure-button">Get over here!</button></div>').addTo(map);
+    var myLayer = L.mapbox.featureLayer().bindPopup('<div class="popup"><p>Hey, Bob</p><button class = "way pure-button">On my way</button><br/><button class = "here pure-button">Get over here!</button></div>').addTo(map);
+
 
     function resetColors() {
         for (var i = 0; i < geoJson.length; i++) {
@@ -124,3 +140,6 @@ var myLayer = L.mapbox.featureLayer().bindPopup('<div class="popup"><p>Hey, Bob<
 
     map.on('click', resetColors);
     myLayer.setGeoJSON(geoJson);
+
+
+}
