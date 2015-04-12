e4a70ee02616a2e0dc3ede5a96558cf8ed23bdb8
unknown <nate.schultz@outlook.com>
2015-04-12 02:09:49 -0400

    work

--- InnerCircle/InnerCircle/scripts/map.js
+++ InnerCircle/InnerCircle/scripts/map.js
@@ -1,6 +1,48 @@
-﻿L.mapbox.accessToken = 'pk.eyJ1IjoibnNjaHVsdHoxNCIsImEiOiJUdXJTc0EwIn0.GDGkKBAERoUmL9RXrmP5bQ';
+﻿
+var userlat;
+var userlong;
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
+}
+function showPosition(position) {
+    userlat = position.coords.latitude;
+    userlong = position.coords.longitude;
+    //latlon = new google.maps.LatLng(lat, lon)
+    //mapholder = document.getElementById('mapholder')
+    ////
+    //var myOptions = {
+    //    center: latlon, zoom: 14,
+    //    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    //    mapTypeControl: false,
+    //    navigationControlOptions: { style: google.maps.NavigationControlStyle.SMALL }
+    //}
+    //var map = new google.maps.Map(document.getElementById("mapholder"), myOptions);
+    //var marker = new google.maps.Marker({ position: latlon, map: map, title: "You are here!" });
+}
+function showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            console.log("User denied the request for Geolocation.")
+            break;
+        case error.POSITION_UNAVAILABLE:
+            console.log("Location information is unavailable.")
+            break;
+        case error.TIMEOUT:
+            console.log("The request to get user location timed out.")
+            break;
+        case error.UNKNOWN_ERROR:
+            console.log("An unknown error occurred.")
+            break;
+    }
+}
+L.mapbox.accessToken = 'pk.eyJ1IjoibnNjaHVsdHoxNCIsImEiOiJUdXJTc0EwIn0.GDGkKBAERoUmL9RXrmP5bQ';
 var map = L.mapbox.map('map', 'examples.h186knp8')
-    .setView([37.9, -77], 6);
+    .setView([userlong, userlat], 6);
 
 var geoJson = [{
     type: 'Feature',
@@ -11,7 +53,7 @@ var geoJson = [{
     properties: {
         title: 'NaN',
         icon: {
-            "iconUrl": "../images/back.png",
+            "iconUrl": "../images/logo.png",
             "iconSize": [0, 0],
             "iconAnchor": [10, 10],
             "popupAnchor": [0, -55],
@@ -30,7 +72,7 @@ var geoJson = [{
     properties: {
         title: 'Sandy',
         icon: {
-            "iconUrl": "../images/back.png",
+            "iconUrl": "../images/logo.png",
             "iconSize": [25, 25],
             "iconAnchor": [10, 10],
             "popupAnchor": [0, -55],
@@ -47,7 +89,7 @@ var geoJson = [{
       "properties": {
           title: 'Bob',
           icon: {
-              "iconUrl": "../images/back.png",
+              "iconUrl": "../images/logo.png",
               "iconSize": [25, 25],
               "iconAnchor": [10, 10],
               "popupAnchor": [0, -55],
@@ -65,7 +107,7 @@ var geoJson = [{
       properties: {
           title: 'Albert',
           icon: {
-              "iconUrl": "../images/back.png",
+              "iconUrl": "../images/logo.png",
               "iconSize": [25, 25],
               "iconAnchor": [10, 10],
               "popupAnchor": [0, -55],
@@ -77,13 +119,13 @@ var geoJson = [{
       type: 'Feature',
       geometry: {
           type: 'Point',
-          coordinates: [-78, 33]
+          coordinates: [userlong, userlat]
       },
       properties: {
           title: 'user',
           icon: {
-              "iconUrl": "../images/back.png",
-              "iconSize": [25, 25],
+              "iconUrl": "../images/logo.png",
+              "iconSize": [50, 50],
               "iconAnchor": [10, 10],
               "popupAnchor": [0, -55],
               "className": "dot"
@@ -114,8 +156,11 @@ myLayer.on('click', function (e) {
     e.layer.feature.properties['old-symbol'] = e.layer.feature.properties['marker-symbol'];
     e.layer.feature.properties['marker-symbol'] = 'circle';
     myLayer.setGeoJSON(geoJson);
-});
 
+    //var marker = e.layer;
+    //marker.setIcon("../images/back.png");
+    //marker.setmage
+});
 
 map.on('click', resetColors);
 myLayer.setGeoJSON(geoJson);
