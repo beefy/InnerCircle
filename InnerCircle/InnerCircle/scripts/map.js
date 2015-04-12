372b52c42abe606dadab27c3ee0a155b6ec650d6
Derek Alonso <futuredominosemployee@gmail.com>
2015-04-12 06:41:50 -0400

    pain and suffereing

--- InnerCircle/InnerCircle/scripts/map.js
+++ InnerCircle/InnerCircle/scripts/map.js
@@ -1,45 +1,7 @@
-﻿
-var userlat;
-var userlong;
+﻿var userlong = 60;
+var userlat = 60;
+var geolocate;
 
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
-    } else {
-        console.log("Geolocation is not supported by this browser.");
-    }
-}
-function showPosition(position) {
-    userlat = position.coords.latitude;
-    userlong = position.coords.longitude;
-    //latlon = new google.maps.LatLng(lat, lon)
-    //mapholder = document.getElementById('mapholder')
-    ////
-    //var myOptions = {
-    //    center: latlon, zoom: 14,
-    //    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    //    mapTypeControl: false,
-    //    navigationControlOptions: { style: google.maps.NavigationControlStyle.SMALL }
-    //}
-    //var map = new google.maps.Map(document.getElementById("mapholder"), myOptions);
-    //var marker = new google.maps.Marker({ position: latlon, map: map, title: "You are here!" });
-}
-function showError(error) {
-    switch (error.code) {
-        case error.PERMISSION_DENIED:
-            console.log("User denied the request for Geolocation.")
-            break;
-        case error.POSITION_UNAVAILABLE:
-            console.log("Location information is unavailable.")
-            break;
-        case error.TIMEOUT:
-            console.log("The request to get user location timed out.")
-            break;
-        case error.UNKNOWN_ERROR:
-            console.log("An unknown error occurred.")
-            break;
-    }
-}
 L.mapbox.accessToken = 'pk.eyJ1IjoibnNjaHVsdHoxNCIsImEiOiJUdXJTc0EwIn0.GDGkKBAERoUmL9RXrmP5bQ';
 var map = L.mapbox.map('map', 'examples.h186knp8')
     .setView([userlong, userlat], 6);
@@ -135,32 +97,30 @@ var geoJson = [{
 
 var myLayer = L.mapbox.featureLayer().bindPopup('<div class="popup"><p>Hey, Bob</p><button class = "way pure-button">On my way</button><br/><button class = "here pure-button">Get over here!</button></div>').addTo(map);
 
-myLayer.setGeoJSON(geoJson);
-
-function resetColors() {
-    for (var i = 0; i < geoJson.length; i++) {
-        geoJson[i].properties['marker-symbol'] = geoJson[i].properties['old-symbol'] ||
-            geoJson[i].properties['marker-symbol'];
+    function resetColors() {
+        for (var i = 0; i < geoJson.length; i++) {
+            geoJson[i].properties['marker-symbol'] = geoJson[i].properties['old-symbol'] ||
+                geoJson[i].properties['marker-symbol'];
+        }
+        myLayer.setGeoJSON(geoJson);
     }
+
+    myLayer.on('layeradd', function (e) {
+        var marker = e.layer,
+            feature = marker.feature;
+        marker.setIcon(L.icon(feature.properties.icon));
+    });
+
+    myLayer.on('click', function (e) {
+        resetColors();
+        e.layer.feature.properties['old-symbol'] = e.layer.feature.properties['marker-symbol'];
+        e.layer.feature.properties['marker-symbol'] = 'circle';
+        myLayer.setGeoJSON(geoJson);
+
+        //var marker = e.layer;
+        //marker.setIcon("../images/back.png");
+        //marker.setmage
+    });
+
+    map.on('click', resetColors);
     myLayer.setGeoJSON(geoJson);
-}
-
-myLayer.on('layeradd', function (e) {
-    var marker = e.layer,
-        feature = marker.feature;
-    marker.setIcon(L.icon(feature.properties.icon));
-});
-
-myLayer.on('click', function (e) {
-    resetColors();
-    e.layer.feature.properties['old-symbol'] = e.layer.feature.properties['marker-symbol'];
-    e.layer.feature.properties['marker-symbol'] = 'circle';
-    myLayer.setGeoJSON(geoJson);
-
-    //var marker = e.layer;
-    //marker.setIcon("../images/back.png");
-    //marker.setmage
-});
-
-map.on('click', resetColors);
-myLayer.setGeoJSON(geoJson);
